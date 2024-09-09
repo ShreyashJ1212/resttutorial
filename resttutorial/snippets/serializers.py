fa7fc3a327116c3b3450c6ee426b7d248c72829c
ShreyashJ1212 <syash12j@gmail.com>
2024-09-09 16:32:06 +0530

    Added snippet code highlights, auth user access. (Upto: Adding Log In To The Browsable API)

--- resttutorial/snippets/serializers.py
+++ resttutorial/snippets/serializers.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.ModelSerializers):
+class SnippetSerializer(serializers.ModelSerializer):
+    owner= serializers.ReadOnlyField(source= "owner.username")
     class Meta:
         model = Snippet
         fields = (
@@ -10,5 +12,13 @@ class SnippetSerializer(serializers.ModelSerializers):
             "code",
             "linenos",
             "language",
-            "style"
+            "style",
+            "owner"
         )
+    
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many= True, queryset= Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "snippets")
