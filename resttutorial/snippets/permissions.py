84012bab4e9754702786c2ab0b71d9f3252b7549
ShreyashJ1212 <syash12j@gmail.com>
2024-09-09 18:16:08 +0530

    Added user permissions, pagination, api to view all other apis, and hyperlinks

---
+++ resttutorial/snippets/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class isOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it
+    """
+    def has_object_permission(self, request, view, obj):
+        # Read permission allowed to any request
+        # so we'll always allow GET, HEAD, or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.owner == request.user
--- resttutorial/snippets/views.py
+++ resttutorial/snippets/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
+from .permissions import isOwnerOrReadOnly
 from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 # Create your views here.
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users":reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format)
+        }
+    )
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset=Snippet.objects.all()
+    renderer_classes=(renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, isOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(owner= self.request.user)
